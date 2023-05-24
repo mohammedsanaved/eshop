d84deecba54d1b989f54fd7bfe073cbe39fb812c
Mohammed Sanaved <mosanaved@gmail.com>
2023-05-24 18:51:01 +0530

    LOGIN & SIGUP FUNCTIONALITY IS ADDED TO BACKEND

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -12,7 +12,7 @@ const getProducts = asyncHandler(async (req, res) => {
 
 //@desc  Fetch single products
 //@route GET /api/products/:id
-//@access Fetch all products
+//@access PUBLIC Fetch all products
 const getProductById = asyncHandler(async (req, res) => {
   const product = await Product.findById(req.params.id);
   if (product) {
